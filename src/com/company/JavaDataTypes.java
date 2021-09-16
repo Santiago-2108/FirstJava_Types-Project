80ee7711ec465174ac7f9d77d2d81f1302902809
James Sule <jamesmosessule@gmail.com>
2021-09-17 00:46:59 +0100

    new task

---
+++ src/com/company/JavaDataTypes.java
@@ -0,0 +1,27 @@
+package com.company;
+
+public class JavaDataTypes {
+    public static void main(String[] args) {
+
+        byte ages = -128;
+        System.out.println("byte variable type = " + ages);
+
+        int age = 12;
+        System.out.println("int variable type = " +age);
+
+        boolean answer = true;
+        System.out.println("Boolean variable type = " +answer);
+
+        short money =  32_766;
+        System.out.println("Short variable type = " + money);
+
+        long VillagePopulation = 2147483646l;
+        System.out.println("Long variable type = " + VillagePopulation);
+
+        float area = 12345678.90f;
+        System.out.println("Float variable type = " + area);
+
+
+
+    }
+}
